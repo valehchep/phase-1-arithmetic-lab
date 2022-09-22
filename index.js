e9240462058e6e7b3c834eab8dda51227e597fc3
Moringa School <valentinecherutich7@email.com>
2022-09-22 17:05:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 =62
+const num2 =1
+const multiply =num1*num2
+const random =(num1)
+Math.random (num1)
+const num3=32
+const num4=7
+const mod=num3%num4
+let max=20
+Math.max=(1,4,6,9,20)
